a1e64cd3811fa9025939abd978a7ac37ed43ea09
knife <ashrafnaiefryan@gmail.com>
2025-03-02 16:17:50 +0530

    Added sorting of homepage

--- frontend/src/DisplayPolls.js
+++ frontend/src/DisplayPolls.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const DisplayPolls = ({ polls }) => {
   const bodyLength = 150;
 
+  const [renderedPolls, setRenderedPolls] = useState(polls);
+  const [sorted, setSorted] = useState("newest");
+
+  const handleChange = (e) => {
+    setSorted(e.target.value);
+  };
+  useEffect(() => {
+    const sortedPolls = renderedPolls.slice().sort((a, b) => {
+      if (sorted === "oldest") {
+        return new Date(a.createdAt) - new Date(b.createdAt);
+      } else if (sorted === "newest") {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      } else if (sorted === "votes") {
+        return (
+          b.options.reduce((acc, curr) => {
+            return acc + curr.optionVotes;
+          }, 0) -
+          a.options.reduce((acc, curr) => {
+            return acc + curr.optionVotes;
+          }, 0)
+        );
+      }
+    });
+
+    setRenderedPolls(sortedPolls);
+  }, [sorted, polls]);
+
   return (
     <div className="polls-grid">
-      {polls.map((poll) => (
+      <div>
+        <label>Sort by:</label>
+        <select name="sort-name" onChange={handleChange}>
+          <option value="newest">Latest</option>
+          <option value="oldest">Oldest</option>
+          <option value="votes">Popularity</option>
+        </select>
+      </div>
+      {renderedPolls.map((poll) => (
         <div className="poll-card" key={poll._id}>
           <Link to={`/poll/${poll._id}`}>
             <div className="poll-header">
@@ -15,7 +50,7 @@ const DisplayPolls = ({ polls }) => {
             </div>
 
             <div className="poll-options">
-              {poll.options.map((option) => (
+              {poll.options.slice(0, 3).map((option) => (
                 <div className="poll-option" key={option.optionNumber}>
                   <button className="option-button">
                     {option.optionValue}
